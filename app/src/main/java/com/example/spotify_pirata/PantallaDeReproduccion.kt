36fe1f0d941e3e53447e7ed37ee9fe164f4b503a
Juan Marcos <juanmarcosleon1990@gmail.com>
2024-01-04 20:32:45 +0000

    canciones nuevas

--- app/src/main/java/com/example/spotify_pirata/PantallaDeReproduccion.kt
+++ app/src/main/java/com/example/spotify_pirata/PantallaDeReproduccion.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
@@ -35,7 +36,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -49,114 +49,118 @@ fun PantallaDeReproduccion(navController: NavHostController) {
     var isShuffleOn by remember { mutableStateOf(false) }
     var isPlaying by remember { mutableStateOf(false) }
 
-Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier.background(color = Color.DarkGray)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .wrapContentSize(Alignment.TopEnd)
     ) {
-
-        Text(
-            text = "Reproduciendo ahora",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
+        Icon(
+            painter = painterResource(id = R.drawable.light_mode_fill0_wght400_grad0_opsz24),
+            contentDescription = "modo claro",
+            tint = Color.White
         )
-        Image(
-            painter = painterResource(id = R.drawable.album_karol),
-            contentDescription = "Album de la karol G",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(350.dp)
-        )
-        Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
-            Text(
-                text = "Nombre de la canción",
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
-            Text(
-                text = "Nombre del artista",
-                color = Color.White
-            )
+    }
+    Column(modifier = Modifier.background(color = Color.DarkGray),
+        verticalArrangement = Arrangement.SpaceEvenly) {
 
-            Column(modifier = Modifier.padding(top = 50.dp)) {
-                Slider(value = 1f, onValueChange = { })
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
+
+                Text(
+                    text = "Reproduciendo ahora",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.album_karol),
+                    contentDescription = "Album de la karol G",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(350.dp)
+                )
+                Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
                     Text(
-                        text = "0:00",
-                        color = Color.White
+                        text = "Nombre de la canción",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
                     )
                     Text(
-                        text = "3:00",
+                        text = "Nombre del artista",
                         color = Color.White
+                    )
+
+                    Column(modifier = Modifier.padding(top = 50.dp)) {
+                        Slider(value = 1f, onValueChange = { })
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "0:00",
+                                color = Color.White
+                            )
+                            Text(
+                                text = "3:00",
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.shuffle_fill0_wght400_grad0_opsz24),
+                        contentDescription = "Random",
+                        tint = if (isShuffleOn) Color.Green else Color.White,
+                        modifier = Modifier.clickable { isShuffleOn = !isShuffleOn }
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrow_back_ios_fill0_wght400_grad0_opsz24),
+                        contentDescription = "Anterior",
+                        tint = Color.White
+                    )
+
+                    val playIcon = rememberVectorPainter(image = Icons.Filled.PlayArrow)
+                    val pauseIcon =
+                        painterResource(id = R.drawable.pause_fill0_wght400_grad0_opsz24)
+
+                    val playPauseIcon = if (isPlaying) pauseIcon else playIcon
+
+                    Icon(
+                        painter = playPauseIcon,
+                        contentDescription = if (isPlaying) "Pause" else "Play",
+                        modifier = Modifier
+                            .clickable {
+                                isPlaying = !isPlaying
+                                exoPlayerViewModel.clicReproducir(contexto = contexto)
+                            }
+                            .size(60.dp),
+                        tint = Color.White,
+
+                        )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrow_forward_ios_fill0_wght400_grad0_opsz24),
+                        contentDescription = "Siguiente",
+                        tint = Color.White
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.repeat_fill0_wght400_grad0_opsz24),
+                        contentDescription = "Repetir",
+                        tint = if (isRepeatOn) Color.Green else Color.White,
+                        modifier = Modifier.clickable { isRepeatOn = !isRepeatOn }
                     )
                 }
             }
         }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
 
-            Icon(
-                painter = painterResource(id = R.drawable.shuffle_fill0_wght400_grad0_opsz24),
-                contentDescription = "Random",
-                tint = if (isShuffleOn) Color.Green else Color.White,
-                modifier = Modifier.clickable { isShuffleOn = !isShuffleOn }
-            )
-
-            Icon(
-                painter = painterResource(id = R.drawable.arrow_back_ios_fill0_wght400_grad0_opsz24),
-                contentDescription = "Anterior",
-                tint = Color.White
-            )
-
-            val playIcon = rememberVectorPainter(image = Icons.Filled.PlayArrow)
-            val pauseIcon = painterResource(id = R.drawable.pause_fill0_wght400_grad0_opsz24)
-
-            val playPauseIcon = if (isPlaying) pauseIcon else playIcon
-
-            Icon(
-                painter = playPauseIcon,
-                contentDescription = if (isPlaying) "Pause" else "Play",
-                modifier = Modifier
-                    .clickable { isPlaying = !isPlaying
-                        exoPlayerViewModel.clicReproducir(contexto = contexto)}
-                    .size(60.dp),
-                tint = Color.White,
-
-                )
-
-            /*     Icon(
-                     imageVector = Icons.Filled.PlayArrow,
-                     contentDescription = "Play",
-                     modifier = Modifier.clickable {
-                         exoPlayerViewModel.crearExoPlayer(context = contexto)
-                         exoPlayerViewModel.reproducir(context = contexto)
-                     },
-                     tint = Color.White
-                 )*/
-
-            Icon(
-                painter = painterResource(id = R.drawable.arrow_forward_ios_fill0_wght400_grad0_opsz24),
-                contentDescription = "Siguiente",
-                tint = Color.White
-            )
-
-            Icon(
-                painter = painterResource(id = R.drawable.repeat_fill0_wght400_grad0_opsz24),
-                contentDescription = "Repetir",
-                tint = if (isRepeatOn) Color.Green else Color.White,
-                modifier = Modifier.clickable { isRepeatOn = !isRepeatOn }
-            )
-        }
-
-
-    }
-}
+//}
