b4ad60cdd5c7add87cf7c6d45331e1f19016db4e
ayozera <rodalvayoformativo@gmail.com>
2024-01-05 16:16:07 +0000

    cargando caratulas y datos del album

--- app/src/main/java/com/example/spotify_pirata/ViewModelListaReproduccion.kt
+++ app/src/main/java/com/example/spotify_pirata/ViewModelListaReproduccion.kt
@@ -20,11 +20,11 @@ class ViewModelListaReproduccion : ViewModel() {
 
     private var _canciones = MutableStateFlow(
         listOf(
-            Cancion("chop_suey", "", "System of a Down"),
-            Cancion("rise_up", "", "TheFatRat"),
-            Cancion("respira_el_momento", "", "Calle 13"),
-            Cancion("pollito_pio", "", "Pulcino Pio"),
-            Cancion("telefono_carpintero", "", "Las ardillitas de Lalo Guerrero"),
+            Cancion("chop_suey", "Toxicity", "System of a Down"),
+            Cancion("rise_up", "Single", "TheFatRat"),
+            Cancion("respira_el_momento", "Multiviral", "Calle 13"),
+            Cancion("pollito_pio", "Il pulcino Pio & friends", "Pulcino Pio"),
+            Cancion("telefono_carpintero", "Hablando con los animales", "Las ardillitas de Lalo Guerrero"),
         )
     )
     val canciones = _canciones.asStateFlow()
